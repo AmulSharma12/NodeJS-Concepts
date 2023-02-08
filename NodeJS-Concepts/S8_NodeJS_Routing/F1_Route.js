3f7e9b6df5f68047c2dcb875f0d5dfbc6dbd6b48
Amul Op <amulsharmarr67@gmail.com>
2023-02-08 22:42:30 +0530

    Routes in NodeJS

---
+++ NodeJS-Concepts/S8_NodeJS_Routing/F1_Route.js
@@ -0,0 +1,20 @@
+//🚀 Routes in NodeJS
+
+// 💫    requiring  the http module.
+const http = require("http");
+
+// 💫    creating server
+const server = http.createServer((req, res) => {
+  if (req.url == "/") res.end("response from the other side");
+  else if (req.url == "/about") res.end("About page");
+  else if (req.url == "/contact") res.end("Contact page");
+  else {
+    res.writeHead(404, { "Content-type": "text/html" });
+    res.end("<h1> Page doesnot exist !</h1>");
+  }
+});
+
+// 💫  for that we have to listen the request as well
+server.listen(3000, "127.0.0.1", () => {
+  console.log("listening to the port 3000");
+});
--- topics.md
+++ topics.md
@@ -5,5 +5,10 @@
 - Repl in NodeJS
 - NodeJS Core Modules
      1. FileSystem Module
+- Import Export own module
+- NPM
+- Nodemon
+- Creating server using NodeJS
+- NodeJS Routing
 
  
